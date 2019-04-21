33a64561ee5a2d6b61bc9d3181ea0971b3eb1d0d
Vujos <vujos.petrovic@gmail.com>
2019-04-22 00:44:52 +0200

    added find study programs candidated for removal

--- src/main/java/App/repositories/StudyProgramRepository.java
+++ src/main/java/App/repositories/StudyProgramRepository.java
@@ -1,11 +1,13 @@
 package App.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import App.models.StudyProgram;
 
 @Repository
 public interface StudyProgramRepository extends JpaRepository<StudyProgram, Long> {
-
+	@Query("SELECT sp FROM StudyProgram sp, SubjectAttendance sa WHERE AVG(sa.finalGrade) <= 7")
+	StudyProgram findStudyProgramsCandidatedForRemoval(StudyProgram studyProgram);
 }
