700fa4ef00e890e16d1c01e4eca8c51b46fd1a7d
Stanko Gavric <stanko.gavric.16@singimail.rs>
2019-06-10 03:46:01 +0200

    finished class schedule

--- backend/src/main/java/App/services/TeachingTermService.java
+++ backend/src/main/java/App/services/TeachingTermService.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import App.models.TeacherRealization;
 import App.models.TeachingTerm;
+import App.repositories.TeacherRealizationRepository;
 import App.repositories.TeachingTermRepository;
 
 @Service
@@ -13,6 +15,8 @@ public class TeachingTermService {
 
 	@Autowired
     private TeachingTermRepository teachingTermRepo;
+	@Autowired
+	private TeacherRealizationRepository teacherRealizationRepo;
 
     public TeachingTermService() {
     }
@@ -30,7 +34,9 @@ public class TeachingTermService {
     }
 
     public void addTeachingTerm(TeachingTerm teachingTerm) {
-        teachingTermRepo.save(teachingTerm);
+    	if(checkTeachingTerm(teachingTerm)) {
+    		teachingTermRepo.save(teachingTerm);
+    	}
     }
     
     public void removeTeachingTerm(Long id) {
@@ -41,11 +47,70 @@ public class TeachingTermService {
     }
 
     public void updateTeachingTerm(Long id, TeachingTerm teachingTerm) {
-        Optional<TeachingTerm> Tea = teachingTermRepo.findById(id);
-        if(Tea.isPresent()) {
-            teachingTerm.setId(Tea.get().getId());
-            teachingTermRepo.save(teachingTerm);
-        }
+    	if(checkTeachingTerm(teachingTerm)) {
+	        Optional<TeachingTerm> Tea = teachingTermRepo.findById(id);
+	        if(Tea.isPresent()) {
+	            teachingTerm.setId(Tea.get().getId());
+	            teachingTermRepo.save(teachingTerm);
+	        }
+    	}
+    }
+    
+    private Boolean checkTeachingTerm(TeachingTerm teachingTerm) {
+    	Iterable<TeacherRealization> teacherRealizations = teacherRealizationRepo.findAll();
+    	Iterable<TeachingTerm> teachingTerms = teachingTermRepo.findAll();
+    	Boolean teacherFree = true;
+    	for(TeacherRealization tr: teacherRealizations) {
+    		if(//teacherRealization.teacher.id == item.teacherRealization.teacher.id &&
+    			tr.getSubjectRealization().getId() == teachingTerm.getSubjectRealization().getId()) {
+	    			teacherFree = false;
+	    				break;
+    		}
+    	}
+    	for(TeachingTerm t : teachingTerms) {
+    		if(
+    			(
+	    			!teacherFree
+	    			|| 
+	    			t.getClassroom().getId() == teachingTerm.getClassroom().getId()
+	    			|| 
+	    			t.getSubjectRealization().getYearOfStudy().getId() == teachingTerm.getSubjectRealization().getYearOfStudy().getId()
+    			)
+    			&&
+    			(
+	    			t.getDay() == teachingTerm.getDay()
+	    			&& 
+	    			(
+	    				(
+	    					t.getStartTime().getTime() > teachingTerm.getStartTime().getTime()
+		    				&&
+		    				t.getStartTime().getTime() < teachingTerm.getEndTime().getTime()
+		    			)
+	    				||
+	    				(
+	    					t.getEndTime().getTime() > teachingTerm.getStartTime().getTime()
+	    					&&
+	    					t.getEndTime().getTime() < teachingTerm.getEndTime().getTime()
+	    				)
+	    				||
+	    				(
+	    					t.getStartTime().getTime() < teachingTerm.getStartTime().getTime()
+	    					&&
+	    					t.getEndTime().getTime() > teachingTerm.getEndTime().getTime()
+	    				)
+	    				||
+	    				(
+	    					t.getStartTime().getTime() == teachingTerm.getStartTime().getTime()
+	    					&&
+	    					t.getEndTime().getTime() == teachingTerm.getEndTime().getTime()
+	    				)
+	    			)
+    			)
+    		) {
+    			return false;
+    		}
+    	}
+    	return true;
     }
 
 }
